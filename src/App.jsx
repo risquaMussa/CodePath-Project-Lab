fb2b9fa75a6df666e11719237db53e57e68e3c37
risquaMussa <rismussa@gmail.com>
2025-06-09 22:16:10 -0400

    second

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
+  );
+}
+export default App;
