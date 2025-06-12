8a018599a2a2f325ff664541eec2fa2c5102c091
risquaMussa <rismussa@gmail.com>
2025-06-11 20:53:53 -0400

    sambmas selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  const updateCount = () => setCount(count + multiplier);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My React App</h1>
-        <p>This is a simple React application.</p>
+      <header className="header">
+        <h1>Samosa Selector</h1>
+        <h2>Count: {count}</h2>
+        <img
+          className="samosa"
+          src="https://courses.codepath.org/course_images/web102/lab2/step1-lab1.png"
+          alt="Samosa"
+          onClick={updateCount}
+        />
       </header>
+      <div className="container">
+        <div className="upgrade">
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button>10 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button>100 samosas</button>
+        </div>
+        <div className="upgrade">
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button>1000 samosas</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 export default App;
